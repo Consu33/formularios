d8fd7571fe902874d2d7506862aa7c0a122c41ae
Consu33 <105826956+Consu33@users.noreply.github.com>
2024-10-24 10:34:34 -0400

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,15 +1,19 @@
 <?php
 
-require 'vendor/autoload.php'; // Autoload de Composer
+//require 'vendor/autoload.php'; // Autoload de Composer
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 
 // Datos del servidor
 $servidor = "localhost";
 $usuario = "root";
 $contraseña = "";
-$baseDeDatos = "formularios";
+$baseDeDatos = "formularios_hospital";
 
 $conexion = new mysqli($servidor, $usuario, $contraseña, $baseDeDatos);
 
@@ -18,7 +22,7 @@ if ($conexion->connect_errno) {
 }
 
 // Función para enviar correo para evento adverso
-function enviarCorreoEventoAdverso($ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servicio, $ev_piso, $ev_ambito, $ev_piso_ambito, $ev_reportar, $ev_descripcion, $ev_acciones, $ev_daño, $ev_notificacion) {
+function enviarCorreoEventoAdverso($ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servicio, $ev_piso, $ev_reporta_ocurrencia, $ev_piso_ambito, $ev_ambito,  $ev_reportar_evento, $ev_descripcion, $ev_acciones, $ev_daño, $ev_daño_descripcion, $ev_notificacion) {
     $mail = new PHPMailer(true);
     try {
         // Configuración del servidor de correo
@@ -26,7 +30,7 @@ function enviarCorreoEventoAdverso($ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servici
         $mail->Host = 'smtp.office365.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'manuel.arrano@redsalud.gob.cl';
-        $mail->Password = 'Jar46967'; // Cambia esta contraseña por la correcta
+        $mail->Password = 'Jar46967'; 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
@@ -41,14 +45,16 @@ function enviarCorreoEventoAdverso($ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servici
         $mail->Body .= "RUT: $ev_rut<br>";
         $mail->Body .= "Ficha: $ev_ficha<br>";
         $mail->Body .= "Fecha: $ev_fecha_ea<br>";
-        $mail->Body .= "Servicio: $ev_servicio<br>";
+        $mail->Body .= "Servicio que reporta: $ev_servicio<br>";
         $mail->Body .= "Piso: $ev_piso<br>";
-        $mail->Body .= "Ámbito: $ev_ambito<br>";
+        $mail->Body .= "Servicio de ocurrencia: $ev_reporta_ocurrencia<br>";
         $mail->Body .= "Piso/Ambito: $ev_piso_ambito<br>";
-        $mail->Body .= "Reportar: $ev_reportar<br>";
+        $mail->Body .= "Ámbito: $ev_ambito<br>";        
+        $mail->Body .= "Eventos a reportar: $ev_reportar_evento<br>";
         $mail->Body .= "Descripción: $ev_descripcion<br>";
         $mail->Body .= "Acciones: $ev_acciones<br>";
         $mail->Body .= "Daño: $ev_daño<br>";
+        $mail->Body .= "Descripcion del daño: $ev_daño_descripcion <br>";
         $mail->Body .= "Notificación: $ev_notificacion<br>";
 
         $mail->send();
@@ -59,7 +65,7 @@ function enviarCorreoEventoAdverso($ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servici
 }
 
 // Función para enviar correo para notificación de caída
-function enviarCorreoNotificacionCaida($nombre, $apellido, $sexo, $edad, $diagnostico_ingreso, $servicio_clinico, $sala, $hora_caida, $dia_caida, $lesiones, $ubicacion_lesion, $descripcion_caida, $sitio_caida, $equipo_mobiliario, $otro_equipo, $entorno, $actividad, $medicamentos_paciente, $estado_paciente, $observaciones) {
+function enviarCorreoNotificacionCaida($nombre, $apellido, $sexo, $edad, $diagnostico_ingreso, $servicio_clinico, $sala, $hora_caida, $dia_caida, $lesiones, $ubicacion_lesion, $descripcion_caida, $sitio_caida, $equipo_mobiliario, $otro_equipo, $entorno, $entorno_timbre, $entorno_iluminacion, $entorno_espacio, $actividad, $medicamentos_paciente, $estado_paciente, $observaciones) {
     $mail = new PHPMailer(true);
     try {
         // Configuración del servidor de correo
@@ -67,7 +73,7 @@ function enviarCorreoNotificacionCaida($nombre, $apellido, $sexo, $edad, $diagno
         $mail->Host = 'smtp.office365.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'manuel.arrano@redsalud.gob.cl';
-        $mail->Password = 'Jar46967'; // Cambia esta contraseña por la correcta
+        $mail->Password = 'Jar46967'; 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
@@ -95,6 +101,9 @@ function enviarCorreoNotificacionCaida($nombre, $apellido, $sexo, $edad, $diagno
         $mail->Body .= "Equipo: $equipo_mobiliario<br>";
         $mail->Body .= "Otro equipo: $otro_equipo<br>";
         $mail->Body .= "Entorno: $entorno<br>";
+        $mail->Body .= "Entorno: $entorno_timbre<br>";
+        $mail->Body .= "Entorno: $entorno_iluminacion<br>";
+        $mail->Body .= "Entorno: $entorno_espacio<br>";
         $mail->Body .= "Actividad: $actividad<br>";
         $mail->Body .= "Medicamentos: $medicamentos_paciente<br>";
         $mail->Body .= "Estado paciente: $estado_paciente<br>";
@@ -117,29 +126,32 @@ if ($formulario === "eventos_adversos") {
     $ev_fecha_ea = $_POST['ev_fecha_ea'];
     $ev_servicio = $_POST['ev_servicio'] ?? '';
     $ev_piso = $_POST['ev_piso'] ?? '';
-    $ev_ambito = $_POST['ev_ambito'] ?? '';
+    $ev_reporta_ocurrencia = $_POST['ev_reporta_ocurrencia'] ?? '';
     $ev_piso_ambito = $_POST['ev_piso_ambito'] ?? '';
-    $ev_reportar = $_POST['ev_reportar'] ?? '';
+    $ev_ambito = $_POST['ev_ambito'] ?? '';    
+    $ev_reportar_evento = $_POST['ev_reportar_evento'] ?? '';
     $ev_descripcion = $_POST['ev_descripcion'] ?? '';
     $ev_acciones = $_POST['ev_acciones'] ?? '';
-    $ev_daño = $_POST['ev_daño'] ?? '';
+    $ev_daño = isset($_POST['ev_daño']) ? $_POST['ev_daño'] : '';
+    $ev_daño_descripcion = isset($_POST['ev_daño_descripcion']) ? $_POST['ev_daño_descripcion'] : '';
     $ev_notificacion = $_POST['ev_notificacion'] ?? '';
 
     // Consulta para la tabla eventos_adversos
-    $consulta_evento = $conexion->prepare("INSERT INTO evento_adversos (ev_rut, ev_ficha, ev_fecha_ea, ev_servicio, ev_piso, ev_ambito, ev_piso_ambito, ev_reportar, ev_descripcion, ev_acciones, ev_daño, ev_notificacion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $consulta_evento = $conexion->prepare("INSERT INTO evento_adversos (ev_rut, ev_ficha, ev_fecha_ea, ev_servicio, ev_piso, ev_reporta_ocurrencia, ev_piso_ambito, ev_ambito, ev_reportar_evento, ev_descripcion, ev_acciones, ev_daño, ev_daño_descripcion, ev_notificacion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-    $consulta_evento->bind_param("ssssssssssss", $ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servicio, $ev_piso, $ev_ambito, $ev_piso_ambito, $ev_reportar, $ev_descripcion, $ev_acciones, $ev_daño, $ev_notificacion);
+    $consulta_evento->bind_param("ssssssssssssss", $ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servicio, $ev_piso, $ev_reporta_ocurrencia, $ev_piso_ambito, $ev_ambito,  $ev_reportar_evento, $ev_descripcion, $ev_acciones, $ev_daño, $ev_daño_descripcion, $ev_notificacion);
 
     if ($consulta_evento->execute()) {
-        echo "Datos de evento_adversos insertados correctamente.";
+        header("Location: http://localhost/formularios_hospital/menu.html");
+        exit;
     } else {
-        echo "Error al insertar datos en evento_adversos: " . $consulta_evento->error;
+        echo "Error al insertar datos en evento adversos: " . $consulta_evento->error;
     }
 
     $consulta_evento->close();
 
-    enviarCorreoEventoAdverso($ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servicio, $ev_piso, $ev_ambito, $ev_piso_ambito, $ev_reportar, $ev_descripcion, $ev_acciones, $ev_daño, $ev_notificacion);
-
+    enviarCorreoEventoAdverso($ev_rut, $ev_ficha, $ev_fecha_ea, $ev_servicio, $ev_reporta_ocurrencia, $ev_piso, $ev_piso_ambito, $ev_ambito, $ev_reportar_evento, $ev_descripcion, $ev_acciones, $ev_daño, $ev_daño_descripcion, $ev_notificacion);
+    
 } elseif ($formulario === "notificaciones_caida") {
     // Declaración de variables de la tabla notificaciones_caida
     $nombre = $_POST['nombre'] ?? '';
@@ -158,35 +170,39 @@ if ($formulario === "eventos_adversos") {
     $equipo_mobiliario = isset($_POST['equipo_mobiliario']) ? implode(', ', $_POST['equipo_mobiliario']) : '';
     $otro_equipo = $_POST['otro_equipo'] ?? '';
     $entorno = isset($_POST['entorno']) ? implode(', ', $_POST['entorno']) : '';
+    $entorno_timbre = isset($_POST['entorno_timbre']) ? implode(', ', $_POST['entorno_timbre']) : '';
+    $entorno_iluminacion = isset($_POST['entorno_iluminacion']) ? implode(', ', $_POST['entorno_iluminacion']) : '';
+    $entorno_espacio = isset($_POST['entorno_espacio']) ? implode(', ', $_POST['entorno_espacio']) : '';
     $actividad = isset($_POST['actividad']) ? implode(', ', $_POST['actividad']) : '';
     $medicamentos_paciente = isset($_POST['medicamentos_paciente']) ? implode(', ', $_POST['medicamentos_paciente']) : '';
     $estado_paciente = isset($_POST['estado_paciente']) ? implode(', ', $_POST['estado_paciente']) : '';
     $observaciones = $_POST['observaciones'] ?? '';
 
     // Consulta para la tabla notificaciones_caida
-    $consulta_paciente = $conexion->prepare("INSERT INTO notificaciones_caida (nombre, apellido, sexo, edad, diagnostico_ingreso, servicio_clinico, sala, hora_caida, dia_caida, lesiones, ubicacion_lesion, descripcion_caida, sitio_caida, equipo_mobiliario, otro_equipo, entorno, actividad, medicamentos_paciente, estado_paciente, observaciones) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $consulta_paciente = $conexion->prepare("INSERT INTO notificaciones_caida (nombre, apellido, sexo, edad, diagnostico_ingreso, servicio_clinico, sala, hora_caida, dia_caida, lesiones, ubicacion_lesion, descripcion_caida, sitio_caida, equipo_mobiliario, otro_equipo, entorno, entorno_timbre, entorno_iluminacion, entorno_espacio, actividad, medicamentos_paciente, estado_paciente, observaciones) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-    $consulta_paciente->bind_param("sssissssssssssssssss", $nombre, $apellido, $sexo, $edad, $diagnostico_ingreso, $servicio_clinico, $sala, $hora_caida, $dia_caida, $lesiones, $ubicacion_lesion, $descripcion_caida, $sitio_caida, $equipo_mobiliario, $otro_equipo, $entorno, $actividad, $medicamentos_paciente, $estado_paciente, $observaciones);
+    $consulta_paciente->bind_param("sssisssssssssssssssssss", $nombre, $apellido, $sexo, $edad, $diagnostico_ingreso, $servicio_clinico, $sala, $hora_caida, $dia_caida, $lesiones, $ubicacion_lesion, $descripcion_caida, $sitio_caida, $equipo_mobiliario, $otro_equipo, $entorno, $entorno_timbre, $entorno_iluminacion, $entorno_espacio, $actividad, $medicamentos_paciente, $estado_paciente, $observaciones);
 
     if ($consulta_paciente->execute()) {
-        echo "Datos de notificaciones_caida insertados correctamente.";
+        header("Location: http://localhost/formularios_hospital/menu.html");
+        exit;
     } else {
-        echo "Error al insertar datos en notificaciones_caida: " . $consulta_paciente->error;
+        echo "Error al insertar datos en notificaciones caida: " . $consulta_paciente->error;
     }
 
     $consulta_paciente->close();
 
-    enviarCorreoNotificacionCaida($nombre, $apellido, $sexo, $edad, $diagnostico_ingreso, $servicio_clinico, $sala, $hora_caida, $dia_caida, $lesiones, $ubicacion_lesion, $descripcion_caida, $sitio_caida, $equipo_mobiliario, $otro_equipo, $entorno, $actividad, $medicamentos_paciente, $estado_paciente, $observaciones);
+    enviarCorreoNotificacionCaida($nombre, $apellido, $sexo, $edad, $diagnostico_ingreso, $servicio_clinico, $sala, $hora_caida, $dia_caida, $lesiones, $ubicacion_lesion, $descripcion_caida, $sitio_caida, $equipo_mobiliario, $otro_equipo, $entorno, $entorno_timbre, $entorno_iluminacion, $entorno_espacio, $actividad, $medicamentos_paciente, $estado_paciente, $observaciones);
+    
 }
 
 $conexion->close();
+
 ?>
 
 
 
 
-<br><br>
 
-<button><a href="eventos_adversos.html">Generar otro formulario Eventos Adversos</a></button>
 
-<button><a href="notificaciones_caida.html">Generar una notificacion de caida</a></button>
+
